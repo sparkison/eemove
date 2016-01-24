d3f489826a6e53efbb8fc90963a65d5673d4942a
Shaun Parkison <shaun.parkison@nerdymind.com>
2016-01-24 08:06:36 -0700

    first attempt to programmatically set sql executable path

--- src/util/EEExtras.java
+++ src/util/EEExtras.java
@@ -30,11 +30,17 @@ public interface EEExtras {
 	public static final String CWD = System.getProperty("user.dir");
 
 	/*
+	 * Determine OS
+	 */
+	public static final String OS = System.getProperty("os.name");
+	
+	/*
 	 * Set the path variable to the mysql/mysqldump commands
 	 * 
 	 * TODO find a better way to do this? Currently will need to compile for
 	 * 		different OS (Mac vs Linux) could reside in multiple different paths
 	 * 		depending on end user system
 	 */
-	public static final String PATH = "/Applications/MAMP/Library/bin/";
+	public static final String PATH = OS.indexOf("mac") >= 0 ? "/Applications/MAMP/Library/bin/" : "/usr/local/bin/";
+	
 }
