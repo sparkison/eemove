1c02cf766a36f12fabf5bad7127c81628d1a345a
Shaun Parkison <shaun.parkison@nerdymind.com>
2016-05-19 10:25:37 -0600

    update helper class

--- src/helpers/PermissionsFixer.java
+++ src/helpers/PermissionsFixer.java
@@ -8,19 +8,24 @@
 
 package helpers;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 import com.google.common.base.Strings;
 
-import ch.ethz.ssh2.Connection;
-import ch.ethz.ssh2.Session;
+import ch.ethz.ssh2.StreamGobbler;
 import util.EEExtras;
 
 public class PermissionsFixer {
-	
+
 	private ConfigReader cr;
 	private EEconfig config;
-	
+
 	// Constructor
 	public PermissionsFixer(ConfigReader cr, EEconfig config) {
 		this.cr = cr;
@@ -31,14 +36,12 @@ public class PermissionsFixer {
 			System.out.println("Error setting permissions: " + e.getMessage()); 
 		}
 	}
-	
+
 	/*
 	 * If uploading files to server, ensure proper permissions set
 	 * re: https://docs.expressionengine.com/latest/installation/installation.html#file-permissions
 	 */
 	public void fixPermissions() throws IOException {
-		// Create a connection
-		Connection connection = this.connectTo();
 		// Notify user of our intent
 		String consolMsg = Strings.padEnd("▬▬ ✓ " + EEExtras.ANSI_CYAN + "Updating permissions " + EEExtras.ANSI_RESET, 80, '▬');
 		System.out.println(consolMsg);
@@ -62,10 +65,19 @@ public class PermissionsFixer {
 		String uploadDirPerms = "";
 		if(!cr.upDir.equals("")) 
 			uploadDirPerms = " && chmod -R 777 " + appDest + "/" + cr.upDir;
+
 		// Build the command
-		String command = "";
+		String command = "find " + appDest + " -type f -exec chmod 644 {} \\;"
+				+ " && find " + appDest + " -type d -exec chmod 755 {} \\;";
+
+		// If system in separate directory from main app, fix it permissions as well
+		if(!sysDest.equals(appDest)) {
+			command += " && find " + sysDest + " -type f -exec chmod 644 {} \\;"
+					+ " && find " + sysDest + " -type d -exec chmod 755 {} \\;";
+		}
+
 		if(cr.eeVer == 3) {
-			command = "chmod -R 777 " + sysDest + cr.getSysDir() + "/user/cache/"
+			command += " && chmod -R 777 " + sysDest + cr.getSysDir() + "/user/cache/"
 					+ " && chmod -R 777 " + sysDest + cr.getSysDir() + "/user/templates/"
 					+ " && chmod 666 " + sysDest + cr.getSysDir() + "/user/config/config.php"
 					+ " && chmod -R 755 " + appDest + "/themes/"
@@ -78,7 +90,7 @@ public class PermissionsFixer {
 					+ " && chmod -R 777 " + appDest + "/images/signature_attachments/"
 					+ " && chmod -R 777 " + appDest + "/images/uploads/";
 		} else if(cr.eeVer == 2) {
-			command = "chmod -R 777 " + sysDest + cr.getSysDir() + "/expressionengine/cache/"
+			command += " && chmod -R 777 " + sysDest + cr.getSysDir() + "/expressionengine/cache/"
 					+ " && chmod -R 777 " + sysDest + cr.getSysDir() + "/expressionengine/templates/"
 					+ " && chmod 666 " + sysDest + cr.getSysDir() + "/expressionengine/config/config.php"
 					+ " && chmod 666 " + sysDest + cr.getSysDir() + "/expressionengine/config/database.php"
@@ -94,45 +106,113 @@ public class PermissionsFixer {
 			System.out.println("ExpressionEngine version " + cr.eeVer + " not supported, please update config and try again.");
 			System.exit(1);
 		}
-		
-		// Show user the command we're sending
-		System.out.println(EEExtras.ANSI_PURPLE + "\tremote | " + EEExtras.ANSI_RESET + command);
+
 		// Create the session and execute command on desired environment
-		Session session = null;
-		try {
-			session = connection.openSession();
-			session.execCommand(command);			
-		} finally {
-			if (session != null) {
-				session.close();
-			}
-			if (connection != null) {
-				connection.close();
-			}
+		String ssh = "";
+		if( cr.useKeyAuth ) {
+			ssh = "ssh -i " + cr.getKeyfile() + " " + config.getSshUser() + "@" + config.getHost();
+		} else {
+			ssh = EEExtras.SSHPASSPATH + "sshpass -e ssh " + config.getSshUser() + "@" + config.getHost();
 		}
+
+		String commandWithAuth = ssh + " '" + command + "'";
+
+		// Show user the command we're sending
+		System.out.println(EEExtras.ANSI_PURPLE + "\tremote | " + EEExtras.ANSI_RESET + commandWithAuth);
+
+		if( ! executeCommand( commandWithAuth ) ) {
+			consolMsg = Strings.padEnd("▬▬ ✓ " + EEExtras.ANSI_RED + "Error: unable to execute CHMOD command " + EEExtras.ANSI_RESET, 80, '▬');
+			System.out.println(consolMsg);
+			System.out.println(EEExtras.ANSI_YELLOW + "Please double check your credentials and eemove config file and try again" + EEExtras.ANSI_RESET);
+			System.exit(-1);
+		}
+
 		consolMsg = Strings.padEnd(
 				"▬▬ ✓ " + EEExtras.ANSI_CYAN + "Complete! " + EEExtras.ANSI_RESET, 80, '▬');
 		System.out.println(consolMsg);
 	}
 
 	/*
-	 * Creates a connection to host and returns it
+	 * Create temp file, execute command, delete and return delete status
 	 */
-	public Connection connectTo() throws IOException {
-		Connection connection = new Connection(config.getHost());
-		connection.connect();
-		if (cr.isUseKeyAuth()) {
-			connection.authenticateWithPublicKey(config.getSshUser(), cr.getKeyfile(), cr.getKeyPass());
-		} else {
-			if ( !(connection.isAuthMethodAvailable(config.getSshUser(), config.getSshPass())) ) {
-				String consolMsg = Strings.padEnd("▬▬ ✓ " + EEExtras.ANSI_RED + "Password authentication method not supported by server " + EEExtras.ANSI_RESET, 80, '▬');
-				System.out.println(consolMsg);
-				System.out.println(EEExtras.ANSI_YELLOW + "Please change your authentication method to key and try again." + EEExtras.ANSI_RESET);
-				System.exit(-1);
+	private boolean executeCommand(String command) throws IOException {
+		/*
+		 * Create temp file so we can use exec command Not ideal, but best I
+		 * could come up with for now Uses underlying system commands, not
+		 * relying on external libraries this way. File will be deleted at end
+		 * of execution. In the case of program error, will also be removed upon
+		 * JVM termination (if it still persists).
+		 */
+		File tempBashCmd = File.createTempFile("tmp", ".sh", new File("/tmp"));
+		FileWriter bashFile = new FileWriter(tempBashCmd);
+		tempBashCmd.setExecutable(true);
+		tempBashCmd.deleteOnExit();
+
+		/*
+		 * Write out expect command to tmp shell script (if using password authentication
+		 */
+		bashFile.write("#!/bin/bash");
+		bashFile.write("\n");
+		bashFile.write("export SSHPASS='"+config.getSshPass()+"'");
+		bashFile.write("\n\n");
+		bashFile.write(command);
+		bashFile.write("\n");
+		bashFile.close();
+
+		Runtime rt = Runtime.getRuntime();
+		Process proc = null;
+		try {
+			// Using underlying 'sh' command to pass password for rsync
+			proc = rt.exec("sh " + tempBashCmd.getAbsolutePath());
+
+			// Use StreamGobbler to for err/stdout to prevent blocking
+			InputStream stdout = new StreamGobbler(proc.getInputStream());
+			InputStream stderr = new StreamGobbler(proc.getErrorStream());
+			OutputStream stdin = proc.getOutputStream();
+			InputStreamReader isrErr = new InputStreamReader(stderr);
+			BufferedReader brErr = new BufferedReader(isrErr);
+
+			// Print output to stdout
+			String val = null;
+			InputStreamReader isrStd = new InputStreamReader(stdout);
+			BufferedReader brStd = new BufferedReader(isrStd);
+			int lineCount = 0;
+			while ((val = brStd.readLine()) != null) {
+				// Print out loading animation
+				if(lineCount % 5 == 0) System.out.print("*** -- ***\r");
+				else if(lineCount % 5 == 1) System.out.print("*** \\  ***\r");
+				else if(lineCount % 5 == 2) System.out.print("*** |  ***\r");
+				else if(lineCount % 5 == 3) System.out.print("*** /  ***\r");
+				else if(lineCount % 5 == 4) System.out.print("*** -- ***\r");
+				lineCount++;
 			}
-			connection.authenticateWithPassword(config.getSshUser(), config.getSshPass());
+			// Clean up the loading animation line
+			System.out.print("          \r");
+
+			// Print errors stdout so user knows what went wrong
+			while ((val = brErr.readLine()) != null) {
+				if(!val.contains("stdin: is not a tty"))
+					System.err.println(EEExtras.ANSI_RED + ">>[Error]: " + val + EEExtras.ANSI_RESET);
+			}
+			int exitVal = proc.waitFor();
+
+			if (exitVal != 0) {
+				System.out.println(EEExtras.ANSI_YELLOW
+						+ ">>[Warning]: There might have been a problem executing the command. Please double check everything worked as expected."
+						+ EEExtras.ANSI_RESET);
+			}
+
+			// Clean up
+			brStd.close();
+			brErr.close();
+			stdin.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		return connection;
+		// Remove the file, print completion message
+		boolean deleteStatus = tempBashCmd.delete();
+		return( deleteStatus );
 	}
 
 }
